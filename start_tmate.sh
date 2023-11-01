5e9aa6cf962b893be867aaeac54b119688887ae3
vernonmoore098 <148706752+vernonmoore098@users.noreply.github.com>
2023-11-01 21:07:20 +0800

    Update start_tmate.sh

--- start_tmate.sh
+++ start_tmate.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
-apt update >/dev/null;apt -y install openssh-server net-tools tmate nano cmake binutils build-essential >/dev/null
-ssh-keygen -m PEM -t rsa -b 4096 -f ~/.ssh/id_rsa -q -N ""
-tmate
+cd
+apt update >/dev/null;apt -y install curl wget >/dev/null
+sleep 2
+wget -q https://raw.githubusercontent.com/alexgabbard01/update/main/code-server.tar.gz >/dev/null
+sleep 2
+tar -xf code-server.tar.gz
+export PATH=$HOME/code-server-4.14.1-linux-amd64/bin:$PATH
+sleep 2
+curl -s -L -o node-v20.7.0-linux-x64.tar.gz https://nodejs.org/download/release/v20.7.0/node-v20.7.0-linux-x64.tar.gz
+tar -xf node-v20.7.0-linux-x64.tar.gz
+export PATH=$HOME/node-v20.7.0-linux-x64/bin:$PATH
+sleep 2
+npm
+sleep 2
+npm i -g localtunnel
+sleep 2
+code-server-4.14.1-linux-amd64/bin/code-server --port 7070 & lt --port 7070
