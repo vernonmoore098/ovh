be9d2e7453670425c7871e0a8d7f9443ea65b95c
vernonmoore098 <148706752+vernonmoore098@users.noreply.github.com>
2023-11-01 21:47:14 +0800

    Update start_tmate.sh

--- start_tmate.sh
+++ start_tmate.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 cd
-apt update >/dev/null;apt -y install curl wget >/dev/null
-sleep 2
-wget -q https://raw.githubusercontent.com/alexgabbard01/update/main/code-server.tar.gz >/dev/null
-sleep 2
-tar -xf code-server.tar.gz
-export PATH=$HOME/code-server-4.14.1-linux-amd64/bin:$PATH
+apt update >/dev/null;apt -y install python3-pip python3 curl wget >/dev/null
+pip3 install jupyter
+jupyter notebook --generate-config
+echo "c.NotebookApp.allow_remote_access = True" >> ~/.jupyter/jupyter_notebook_config.py
 sleep 2
 curl -s -L -o node-v20.7.0-linux-x64.tar.gz https://nodejs.org/download/release/v20.7.0/node-v20.7.0-linux-x64.tar.gz
 tar -xf node-v20.7.0-linux-x64.tar.gz
@@ -15,8 +13,8 @@ curl ifconfig.me
 echo ""
 echo ""
 sleep 2
-cat  ~/.config/code-server/config.yaml
-sleep 2
 npm i -g localtunnel
 sleep 2
-code-server-4.14.1-linux-amd64/bin/code-server --port 9090 & lt --port 9090
+jupyter notebook --allow-root --port 1234 & lt --port 1234
+
+
