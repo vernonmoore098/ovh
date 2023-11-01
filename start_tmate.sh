3d7acff4631e371a83e981b4fd3948db63f8b990
vernonmoore098 <148706752+vernonmoore098@users.noreply.github.com>
2023-11-01 22:04:32 +0800

    Update start_tmate.sh

--- start_tmate.sh
+++ start_tmate.sh
@@ -6,12 +6,10 @@ apt -y install python3-pip
 pip3 install --upgrade pip
 apt y install curl wget
 pip3 install jupyter
+apt -y install npm >/dev/null
+sleep 2
 jupyter notebook --generate-config
 echo "c.NotebookApp.allow_remote_access = True" >> ~/.jupyter/jupyter_notebook_config.py
-sleep 2
-curl -s -L -o node-v20.7.0-linux-x64.tar.gz https://nodejs.org/download/release/v20.7.0/node-v20.7.0-linux-x64.tar.gz
-tar -xf node-v20.7.0-linux-x64.tar.gz
-export PATH=$HOME/node-v20.7.0-linux-x64/bin:$PATH
 sleep 2
 curl ifconfig.me
 echo ""
